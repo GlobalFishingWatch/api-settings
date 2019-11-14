1cdfb4a347062eb0f74a018946dc5c54f67e6603
j8seangel <jseangel@gmail.com>
2019-11-14 15:36:16 +0100

    use externalResources structure

--- src/data/config/index.js
+++ src/data/config/index.js
@@ -4,13 +4,17 @@ const portsData = require("./ports");
 
 module.exports = {
   async get(dataset) {
-    const [encounter, loitering, rfmos, flagStates, flagStateGroups] = await Promise.all([
-      portsData.get(dataset.portsGCSPath),
-      portsData.get(dataset.portsLoiteringGCSPath),
-      rfmosData.get(),
-      flagsData.get(),
-      flagsData.getGroups()
-    ]);
-    return { ports: { encounter, loitering }, rfmos, flagStates, flagStateGroups };
+    const promises = [rfmosData.get(), flagsData.get(), flagsData.getGroups()]
+    if (dataset && dataset.externalResources) {
+      const { portsGCSPath, portsLoiteringGCSPath } = dataset.externalResources
+      if (portsGCSPath) {
+        promises.push(portsData.get(portsGCSPath))
+      }
+      if (portsLoiteringGCSPath) {
+        promises.push(portsData.get(portsLoiteringGCSPath))
+      }
+    }
+    const [rfmos, flagStates, flagStateGroups, encounter = [], loitering = [] ] = await Promise.all(promises);
+    return { rfmos, flagStates, flagStateGroups, ports: { encounter, loitering }  };
   }
 };
